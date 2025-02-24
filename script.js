8e54090e997ea7e519e6301a64a4b75040947698
Manish Kumar Mittal <98324095+Manish636700@users.noreply.github.com>
2025-02-25 02:55:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function makeid(l) {
-  // write your code here
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+
+	for(let i=0; i<l; i++)
+		{
+			result = result+characters.charAt(Math.floor(Math.random()*characters.length));
+		}
+		return result;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+// const l = prompt("Enter a number.");
+// alert(makeid(l));
